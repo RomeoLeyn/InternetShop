d9ee4982d1f057e92e0ea6853c768e68915fc2e0
RomeoLeyn <romanpituh228@gmail.com>
2023-06-30 13:40:19 +0300

    new feature

---
+++ js/addToCart.js
@@ -0,0 +1,88 @@
+// const { response } = require("express");
+
+// function addToCart(id) {
+//     let cartBody = document.querySelector('modal-body-cart');
+
+//     fetch('http://localhost:3000/js/addToCart', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             _id: id
+//         })
+//             .then(response => {
+//                 if (response.ok) {
+//                     console.log('cart add successfully');
+//                     window.location.href = "main.html";
+//                 } else {
+//                     console.error('Error add cart');
+//                 }
+//             })
+//             .catch(error => {
+//                 console.error('Error add cart:', error);
+//             })
+//     })
+
+//     let infoOfProductInCartBody = `<table class="table">
+
+
+//     <thead>
+//         <tr>
+//             <th scope="col">#</th>
+//             <th scope="col"></th>
+//             <th>Виробник/Модель</th>
+//             <th scope="col">Тип (Wi Мережева карта GbE)</th>
+//             <th scope="col">Максимальна швидкість з'єднання, Мбіт/с</th>
+//             <th scope="col">Розміри, мм</th>
+//         </tr>
+//     </thead>
+
+//     <tbody>
+//         <tr>
+//             <th scope="row"><input type="checkbox">1</th>
+//             <td><img src="images/router2.jpg" alt=""></td>
+//             <td>${product.name}</td>
+//             <td> ${product.typeConnect}</td>
+//             <td>${product.maxSpeed}</td>
+//             <td>${product.productSize}</td>
+//         </tr>
+
+//     </tbody>
+// </table>`
+
+
+//     cartBody.innerHTML += infoOfProductInCartBody;
+// }
+
+function addToCart(id) {
+    let cartBody = document.querySelector('.modal-body-cart');
+    const nameProduct = document.getElementById('name-product').textContent;
+    const priceProduct = document.querySelector('.product-event-price').textContent;
+
+    let cartBlock = `
+     <tbody>
+     <table class="table">
+
+             <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col"></th>
+                        <th>Виробник/Модель</th>
+                        <th scope="col">Тип (Wi Мережева карта GbE)</th>
+                        <th scope="col">Максимальна швидкість з'єднання, Мбіт/с</th>
+                        <th scope="col">Розміри, мм</th>
+                      </tr>
+            </thead>
+         <tr>
+             <th scope="row"><input type="checkbox">1</th>
+             <td><img src="images/router2.jpg" alt=""></td>
+             <td>${nameProduct}</td>
+             <td> ${priceProduct}</td>
+         </tr>
+     </tbody>
+ </table>`
+
+
+    cartBody.innerHTML += cartBlock;
+}
